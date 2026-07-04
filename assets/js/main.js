4d5d360a969b36448195cd582f5df50227eb7ff8
7-wwh <lamweiheng124@gmail.com>
2026-07-04 09:49:47 +0000

    Update navigation bar and add theme switcher

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,31 @@
     });
   }
 
+  // Theme Switcher Logic
+  const switcher = document.querySelector('.switcher');
+  if(switcher) {
+    const trackPrevious = (el) => {
+      const radios = el.querySelectorAll('input[type="radio"]');
+      let previousValue = null;
+
+      const initiallyChecked = el.querySelector('input[type="radio"]:checked');
+      if (initiallyChecked) {
+        previousValue = initiallyChecked.getAttribute("c-option");
+        el.setAttribute('c-previous', previousValue);
+      }
+
+      radios.forEach(radio => {
+        radio.addEventListener('change', () => {
+          if (radio.checked) {
+            el.setAttribute('c-previous', previousValue ?? '');
+            previousValue = radio.getAttribute("c-option");
+          }
+        });
+      });
+    }
+    trackPrevious(switcher);
+  }
+
   // Burn Rail (Scroll Progress)
   var fill = document.getElementById('burnedFill');
   if(!fill) return;
